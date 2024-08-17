f50aae6b42604b06af4abd9569d1d9249693226e
Flavio Vieira Leao <flavio.vl@gmail.com>
2024-08-17 20:25:24 -0300

    perf: otimiza script de start em modo desenvolvimento

---
+++ scripts/start_dev.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e errexit
+set -e pipefail
+set -e nounsetcle
+set -e xtrace
+
+C='\e[0;36m'     # cyan
+Y='\033[0;33m'   # yellow
+G='\033[0;32m'   # green 
+B='\033[0;34m'   # blue
+W='\033[0;37m'   # white
+E='\033[0m'      # end
+
+echo "${C}_________________________________________________________________________________________________________${E}"
+echo "${C}=> CHECK INTEGRITY (check)                                                                               ${E}"
+python manage.py check
+
+echo "${C}_________________________________________________________________________________________________________${E}"
+echo "${C}=> STARTING CRON                                                                                         ${E}"
+echo 'Cronjobs mec-cron in operating system'
+/bin/cp /home/dev/mec-energia-api/cronjob/cronjob /etc/cron.d/mec-cron
+crontab /etc/cron.d/mec-cron
+cron
+
+echo "${C}_________________________________________________________________________________________________________${E}"
+echo "${C}=> MAKING MIGRATIONS (makemigrations)                                                                    ${E}" 
+python manage.py makemigrations
+
+echo "${C}_________________________________________________________________________________________________________${E}"
+echo "${C}=> APPLYING MIGRATIONS (migrate)                                                                         ${E}"
+python manage.py migrate
+
+echo "${C}_________________________________________________________________________________________________________${E}"
+echo '\e[33;36m=> RUNNING SERVER (runserver) \e[0;33m                                                                  '
+echo " ${Y}                                                     __    ${E}   ______  _____________________ _______     "
+echo " ${Y}                                                   /    \  ${E}   ___   |/  /___  ____/___  __ \___    |    "
+echo " ${Y}                                                  /__    \ ${E}   __  /|_/ / __  __/   __  /_/ /__  /| |    "
+echo " ${Y}                                                  \  /\  / ${E}   _  /  / /  _  /___   _  ____/ _  ___ |    "
+echo " ${Y}                                                   \/__\/  ${E}   /_/  /_/   /_____/   /_/      /_/  |_|    "
+echo "Starting ${W}"${API_SERVICE_NAME}"${E} - ${G}"${API_URL}"${E}                            ${B}Consumo de energia com sabedoria${E} "
+echo "${C}_________________________________________________________________________________________________________${E}"
+python manage.py runserver ${API_HOST}:${API_PORT}
