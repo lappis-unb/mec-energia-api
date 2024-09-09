8285da6cd70f668f1d1ea035171d4c6c7d7a97fe
Daniel Porto <danielportods@gmail.com>
2024-09-09 20:19:40 -0300

    Incrementa a seed das tarifas

--- universities/models.py
+++ universities/models.py
@@ -14,7 +14,7 @@ from utils.subgroup_util import Subgroup
 
 class University(models.Model):
     name = models.CharField(
-        max_length=50,
+        max_length=100,
         blank=False,
         null=False,
         unique=True,
@@ -50,7 +50,7 @@ class University(models.Model):
 
 class ConsumerUnit(models.Model):
     name = models.CharField(
-        max_length=50,
+        max_length=100,
         verbose_name=_('Nome'),
         help_text=_('Nome da Unidade Consumidora. Ex: Darcy Ribeiro')
     )
